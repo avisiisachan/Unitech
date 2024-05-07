ec415fccaf2de2712b4b892cb5654332bcb80592
avisiisachan <164173780+avisiisachan@users.noreply.github.com>
2024-05-07 16:40:12 +0530

    Update FeedbackCard.tsx

--- app/components/Reviews/FeedbackCard.tsx
+++ app/components/Reviews/FeedbackCard.tsx
@@ -3,10 +3,17 @@
 import React from 'react';
 import './FeedbackCard.styles.css'; // Import CSS file
 
-const FeedbackCard = ({ content, name, title, img }) => (
+interface FeedbackCardProps {
+  content: string;
+  name: string;
+  title: string;
+  img: string;
+}
+
+const FeedbackCard: React.FC<FeedbackCardProps> = ({ content, name, title, img }) => (
   <div className="feedback-card-container">
     <div className="feedback-card">
-    <div className="flex flex-row">
+      <div className="flex flex-row">
         <img src={img} alt={name} className="rounded-full" />
         <div className="flex flex-col ml-4">
           <h4 className="font-poppins font-semibold text-[20px] leading-[32px]">
@@ -20,8 +27,6 @@ const FeedbackCard = ({ content, name, title, img }) => (
       <p className="font-poppins font-normal text-[18px] leading-[32.4px]">
         {content}
       </p>
-
-      
     </div>
   </div>
 );
